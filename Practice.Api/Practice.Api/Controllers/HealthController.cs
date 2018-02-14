ba2eafce43fa0bdda864d53feba7f13e895bbb02
PracticeExercises <33603571+PracticeExercises@users.noreply.github.com>
2018-02-14 18:05:54 +0100

    Add a basic API project.

---
+++ Practice.Api/Practice.Api/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+
+namespace Practice.Api.Controllers
+{
+    [RoutePrefix("api")]
+    public class HealthController : ApiController
+    {
+        [Route("health")]
+        public string CheckHealth() => "OK";
+    }
+}
