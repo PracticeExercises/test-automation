d14d102a9226d01d8afdac0217481041c2f5aaf0
PracticeExercises <33603571+PracticeExercises@users.noreply.github.com>
2018-02-15 09:37:16 +0100

    Mark view model properties as required.

--- Practice.Api/Practice.Api/Models/CalculateViewModel.cs
+++ Practice.Api/Practice.Api/Models/CalculateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace Practice.Api.Models
 {
@@ -6,10 +7,18 @@ namespace Practice.Api.Models
     public class CalculateViewModel
     {
         [DataMember(Name = "principal")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(1, 1000000.0, ErrorMessage = "{0} out of range")]
         public double? Principal { get; set; }
+
         [DataMember(Name = "percentageRate")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(0.0, 100.0, ErrorMessage = "{0} out of range")]
         public double? Rate { get; set; }
+
         [DataMember(Name = "years")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(1, 30, ErrorMessage = "{0} out of range")]
         public int? Years { get; set; }
     }
 }
